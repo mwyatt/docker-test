ed878aae7a82f09ff04c9b10d7231dd36e144479
Martin Wyatt <martin.wyatt@gmail.com>
2019-05-24 16:41:16 +0100

    updates

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-// const mongoose = require('mongoose')
 
 // Constants
 const PORT = 8080;
@@ -13,8 +12,32 @@ const app = express();
 // app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  console.log('console.log hi')
-  res.send('res.send hi');
+  console.log(req, res)
+  res.send('res.send hi2');
+});
+
+app.get('/test/', (req, res) => {
+  console.log('testing')
+  res.send('testing');
+});
+
+app.get('/mongo/', (req, res) => {
+    var MongoClient = require('mongodb').MongoClient
+
+    MongoClient.connect('mongodb://localhost:27017/mydb', function (err, db) {
+      if (err) {
+        console.log(err)
+      }
+        console.log(1)
+        console.log(db)
+      // db.collection('cities').find().toArray(function (err, result) {
+      //   if (err) throw err
+
+      //   console.log(result)
+      // })
+    })
+  res.send('testing');
+
 });
 
 app.listen(PORT, HOST);
